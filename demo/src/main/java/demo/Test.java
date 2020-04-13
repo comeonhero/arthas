a9eba0e1ae23aafde1d3a556f8350832c2430b6c
sunqi05 <sunqi05@ppdai.com>
2020-04-13 18:30:37 +0800

    测试5

---
+++ demo/src/main/java/demo/Test.java
@@ -0,0 +1,20 @@
+package demo;
+
+/**
+ * 测试dev
+ *
+ * @author sunqi
+ * @since 2020/4/13
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("测试==========");
+        System.out.println("测试==========1");
+        System.out.println("测试==========2");
+        System.out.println("测试==========3");
+        System.out.println("测试==========4");
+        System.out.println("测试==========5");
+    }
+
+}
